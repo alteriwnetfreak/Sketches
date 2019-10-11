8745ed69041bd30138b331080a82d441c513a53b
Dieter Koert <dieterkoert@gmail.com>
2019-10-11 17:50:40 +0200

    Meer tests toegevoegd
    Is tot nu toe goed gegaan

--- TestDisplay/TestDisplay.ino
+++ TestDisplay/TestDisplay.ino
@@ -1,12 +1,26 @@
+//----------------------------------------------------
+// Display test voor Arduino
+//----------------------------------------------------
+
+// include the library code:
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  lcd.begin(0, 0);
-  lcd.print("hello, world!");
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Het is gelukt!");
 }
 
 void loop() {
-  
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 100);
 }
