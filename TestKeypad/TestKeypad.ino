d327395cf9bc263fc51bbca6e1d5153110b66ac3
Dieter Koert <dieterkoert@gmail.com>
2019-10-16 17:47:09 +0200

    Keypad test
    Is succesvol,
    met wachtwoord

--- TestKeypad/TestKeypad.ino
+++ TestKeypad/TestKeypad.ino
@@ -2,12 +2,97 @@
 // Keypad test
 //----------------------------------------------------
 
-void setup() {
-  // put your setup code here, to run once:
+// Including keypad
+#include <Keypad.h>
 
+const byte ROWS = 4; 
+const byte COLS = 3; 
+
+char hexaKeys[ROWS][COLS] = {
+	{'1', '2', '3'},
+	{'4', '5', '6'},
+	{'7', '8', '9'},
+	{'*', '0', '#'}
+};
+
+//On keypad-board, from left to right
+//Connect to pins: 7 to 13
+//   -->      /////// /
+//          -------------
+//          | 1   2   3 |
+//          |           |
+//          | 4   5   6 |
+//          |           |
+//          | 7   8   9 |
+//          |           |
+//          | *   0   # |
+//          -------------
+byte colPins[COLS] = {7, 8, 9}; 
+byte rowPins[ROWS] = {10, 11, 12, 13}; 
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+
+// Global Variables
+int dataCount = 0;
+#define passwordLength 6
+
+char data[passwordLength] = "";
+char passWord[passwordLength] = "21199";
+char passWordReset[passwordLength] = "2#111";
+
+bool passwordBeingReset = false;
+
+
+void setup(){
+	Serial.begin(9600);
+	Serial.println("Voer wachtwoord in: ");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+	giveData();
 
+	if(!passwordBeingReset) {
+		if(dataCount == passwordLength - 1) {
+			if(!strcmp(data, passWord)) {
+				Serial.println("Correct!");
+			} else if(!strcmp(data, passWordReset)) {
+				Serial.println("New Pass: ");
+				passwordBeingReset = !passwordBeingReset;
+			} else {
+				Serial.println("Incorrect!");
+				Serial.println("Try Again...");
+			}
+			clearData();
+		}
+	} else {
+		for(int i = 0; i < passwordLength - 1; i++) {
+			passWord[i] = data[i];
+		}
+		if(dataCount == passwordLength - 1) {
+			Serial.print("New Pass: ");
+			Serial.println(passWord);
+			clearData();
+			passwordBeingReset = !passwordBeingReset;
+		}
+	}
 }
+
+char* giveData() {
+	char customKey = customKeypad.getKey();
+	if(customKey) {
+		data[dataCount] = customKey;
+		dataCount++;
+		Serial.println(data);
+	}
+	return data;
+}
+
+void clearData() {
+	for(int i = 0; i < passwordLength; i++) {
+		data[i] = 0;
+	}
+	dataCount = 0;
+}
+/*
+*/
